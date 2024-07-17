ae8c9a2d2c53c9e0eae692de839574d3a445b3cb
AM03688 <Daniel.moody@carelon.com>
2024-07-17 17:39:31 +0100

    can now view movies in list on POST request

--- movie-man-view/src/components/MoviesList.js
+++ movie-man-view/src/components/MoviesList.js
@@ -1,6 +1,5 @@
-
-import React,{useEffect, useState} from 'react';
 import axios from 'axios';
+import React,{useEffect, useState} from 'react';
 
 const MoviesList = () => {
     const[movies,setMovies] = useState([]);
