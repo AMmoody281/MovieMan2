69ff8a6fe5d3456d0a46f30b2220e91df2a1fb34
AM03688 <daniel.moody@carelon.com>
2024-08-16 16:56:11 +0100

    all

--- MovieMan/src/main/java/com/MovieMan/MovieMan/Repository/CustomMovieRepo.java
+++
@@ -1,8 +0,0 @@
-package com.MovieMan.MovieMan.Repository;
-
-import com.MovieMan.MovieMan.Model.MovieEntity;
-import com.MovieMan.MovieMan.Model.ReviewEntity;
-
-public interface CustomMovieRepo {
-    MovieEntity addReviewToMovie(String movieId, ReviewEntity review);
-}
